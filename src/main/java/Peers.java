4290053e59d18690cf053727d669a4a85144a84c
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-18 18:45:30 +0200

    Piece Hashes

--- src/main/java/Peers.java
+++ src/main/java/Peers.java
@@ -17,7 +17,7 @@ public class Peers {
     }
 
     public void discoverPeers() throws Exception {
-        String infoHash = URLEncoder.encode(new String(torrent.infoHash, StandardCharsets.ISO_8859_1), StandardCharsets.ISO_8859_1.toString());
+        String infoHash = urlEncode(torrent.infoHash);
         String peerId = "00112233445566778899";
         int port = 6881;
         int uploaded = 0;
@@ -45,7 +45,7 @@ public class Peers {
             Map<String, Object> responseMap = bencode.decode(response.toString().getBytes(StandardCharsets.ISO_8859_1), Type.DICTIONARY);
             System.out.println("Response: " + responseMap);
 
-            byte[] peers = (byte[]) responseMap.get("peers");
+            byte[] peers = ((String) responseMap.get("peers")).getBytes(StandardCharsets.ISO_8859_1);
             for (int i = 0; i < peers.length; i += 6) {
                 String ip = String.format("%d.%d.%d.%d", (peers[i] & 0xff), (peers[i + 1] & 0xff), (peers[i + 2] & 0xff), (peers[i + 3] & 0xff));
                 int peerPort = ((peers[i + 4] & 0xff) << 8) | (peers[i + 5] & 0xff);
@@ -54,5 +54,17 @@ public class Peers {
         } else {
             System.out.println("Failed to get response from tracker");
         }
+    }
+
+    private String urlEncode(byte[] bytes) {
+        StringBuilder encoded = new StringBuilder();
+        for (byte b : bytes) {
+            if ((b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9') || b == '-' || b == '_' || b == '.' || b == '~') {
+                encoded.append((char) b);
+            } else {
+                encoded.append(String.format("%%%02X", b));
+            }
+        }
+        return encoded.toString();
     }
 }
