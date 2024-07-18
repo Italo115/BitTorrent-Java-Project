8a768e9a06ef26a34f566830eea3975a0822d63c
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-18 17:22:00 +0200

    Calculate info hash

--- src/main/java/TorrentInfo.java
+++ src/main/java/TorrentInfo.java
@@ -3,46 +3,22 @@ import com.dampcake.bencode.Type;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 class TorrentInfo {
     public String announce;
     public long length;
     public byte[] infoHash;
-    public long pieceLength;
-    public List<String> pieceHashes;
 
     @SuppressWarnings("unchecked")
     public TorrentInfo(byte[] bytes) throws NoSuchAlgorithmException {
-        Bencode bencode = new Bencode(false);
-        Map<String, Object> root = bencode.decode(bytes, Type.DICTIONARY);
+        Bencode bencode1 = new Bencode(false);
+        Bencode bencode2 = new Bencode(true);
+        Map<String, Object> root = bencode1.decode(bytes, Type.DICTIONARY);
         Map<String, Object> info = (Map<String, Object>) root.get("info");
-
         announce = (String) root.get("announce");
-        length = ((Number) info.get("length")).longValue();
-        pieceLength = ((Number) info.get("piece length")).longValue();
-
-        String piecesString = (String) info.get("pieces");
-        byte[] piecesBytes = piecesString.getBytes();
-        pieceHashes = new ArrayList<>();
-        for (int i = 0; i < piecesBytes.length; i += 20) {
-            byte[] hash = Arrays.copyOfRange(piecesBytes, i, i + 20);
-            pieceHashes.add(bytesToHex(hash));
-        }
-
+        length = (long) info.get("length");
         MessageDigest digest = MessageDigest.getInstance("SHA-1");
-        infoHash = digest.digest(bencode.encode((Map<String, Object>) info));
-    }
-
-    private static String bytesToHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            sb.append(String.format("%02x", b));
-        }
-        return sb.toString();
+        infoHash = digest.digest(bencode2.encode((Map<String, Object>) bencode2.decode(bytes, Type.DICTIONARY).get("info")));
     }
 }
