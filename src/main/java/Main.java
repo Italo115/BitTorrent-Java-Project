9b18de70bae8942f7d749b561acd4e48565721a4
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-18 18:36:39 +0200

    Piece Hashes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,9 @@ import com.dampcake.bencode.Bencode;
 import com.dampcake.bencode.Type;
 import com.google.gson.Gson;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+
 
 public class Main {
     private static final Gson gson = new Gson();
