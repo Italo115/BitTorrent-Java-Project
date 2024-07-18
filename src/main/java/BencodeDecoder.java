cc25395c257e24582691b99a8156e8a1c22f3ffa
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-18 16:30:09 +0200

    Calculate info hash

--- src/main/java/BencodeDecoder.java
+++ src/main/java/BencodeDecoder.java
@@ -126,15 +126,15 @@ public class BencodeDecoder {
         String announce = (String) decodedDictionary.get("announce");
         Map<String, Object> info = (Map<String, Object>) decodedDictionary.get("info");
         Long length = (Long) info.get("length");
-        System.out.println("Info goes here : ----- " + decodedDictionary.entrySet());
-        System.out.println("Tracker URL: " + announce);
-        System.out.println("Length: " + length);
+        System.out.println("Info goes here : ----- " + decodedDictionary.get(announce));
+        //System.out.println("Tracker URL: " + announce);
+        //System.out.println("Length: " + length);
 
         byte[] bencodedInfo = bencode(info);
         MessageDigest sha1Digest = MessageDigest.getInstance("SHA-1");
         byte[] infoHash = sha1Digest.digest(bencodedInfo);
 
-        System.out.println("Info Hash: " + bytesToHex(infoHash));
+        // System.out.println("Info Hash: " + bytesToHex(infoHash));
     }
 
     private static String bytesToHex(byte[] bytes) {
