970306ee0da58b7ada91b452cf2b435666614f7b
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-18 14:54:32 +0200

    Decode bencoded String

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -36,11 +36,7 @@ public class Main {
       int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
       return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
     } else if (bencodedString.charAt(0) == 'i') {
-      System.out.println("Full string bencode here:  "+bencodedString);
-      //Then it is an integer
-      //Thus we now do the bencoding for integer
       int length = bencodedString.length() -1;
-      System.out.println("Bencoded Fix :  "+bencodedString.substring(1,length));
         return bencodedString.substring(1,length);
 
 
