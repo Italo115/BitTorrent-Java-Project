4d9dccc98be69ee145d15aa2b7daab54185333b8
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-18 17:11:41 +0200

    Calculate info hash

--- src/main/java/TorrentInfo.java
+++ src/main/java/TorrentInfo.java
@@ -3,12 +3,16 @@ import com.dampcake.bencode.Type;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 class TorrentInfo {
     public String announce;
     public long length;
     public byte[] infoHash;
+    public long pieceLength;
+    public List<String> pieceHashes;
 
     @SuppressWarnings("unchecked")
     public TorrentInfo(byte[] bytes) throws NoSuchAlgorithmException {
@@ -18,7 +22,23 @@ class TorrentInfo {
         Map<String, Object> info = (Map<String, Object>) root.get("info");
         announce = (String) root.get("announce");
         length = (long) info.get("length");
+        pieceLength = (long) info.get("piece length");
+        byte[] piecesBytes = (byte[]) info.get("pieces");
+        pieceHashes = new ArrayList<>();
+        for (int i = 0; i < piecesBytes.length; i += 20) {
+            byte[] hash = new byte[20];
+            System.arraycopy(piecesBytes, i, hash, 0, 20);
+            pieceHashes.add(bytesToHex(hash));
+        }
         MessageDigest digest = MessageDigest.getInstance("SHA-1");
         infoHash = digest.digest(bencode2.encode((Map<String, Object>) bencode2.decode(bytes, Type.DICTIONARY).get("info")));
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
     }
 }
