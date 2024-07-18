d9671f4b97d46f5554a1fee70b3fb6430a568076
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-18 17:17:33 +0200

    Calculate info hash

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,65 +2,41 @@ import com.dampcake.bencode.Bencode;
 import com.dampcake.bencode.Type;
 import com.google.gson.Gson;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class Main {
     private static final Gson gson = new Gson();
 
     public static void main(String[] args) throws Exception {
         String command = args[0];
-        if (command.equals("decode")) {
+        if ("decode".equals(command)) {
             String bencodedValue = args[1];
-            String decoded;
-            switch (bencodedValue.charAt(0)) {
-                case 'i' -> {
-                    Bencode bencode = new Bencode(true);
-                    decoded = "" + bencode.decode(bencodedValue.getBytes(), Type.NUMBER);
-                }
-                case 'l' -> {
-                    Bencode bencode = new Bencode(false);
-                    decoded = gson.toJson(bencode.decode(bencodedValue.getBytes(), Type.LIST));
-                }
-                case 'd' -> {
-                    Bencode bencode = new Bencode(false);
-                    decoded = gson.toJson(bencode.decode(bencodedValue.getBytes(), Type.DICTIONARY));
-                }
-                default -> {
-                    try {
-                        decoded = gson.toJson(decodeBencode(bencodedValue));
-                    } catch (RuntimeException e) {
-                        System.out.println(e.getMessage());
-                        return;
-                    }
-                }
+            BencodeDecoder decoder = new BencodeDecoder(bencodedValue.getBytes(StandardCharsets.UTF_8));
+            Object decoded;
+            try {
+                decoded = decoder.decode();
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                return;
             }
-            System.out.println(decoded);
-        } else if (command.equals("info")) {
-            String filePath = args[1];
-            TorrentInfo torrent = new TorrentInfo(Files.readAllBytes(Path.of(filePath)));
+            System.out.println(gson.toJson(decoded));
+        } else if ("info".equals(command)) {
+            byte[] res = Files.readAllBytes(Path.of(args[1]));
+            TorrentInfo torrent = new TorrentInfo(res);
             System.out.println("Tracker URL: " + torrent.announce);
             System.out.println("Length: " + torrent.length);
             System.out.println("Info Hash: " + bytesToHex(torrent.infoHash));
+            System.out.println("Piece Length: " + torrent.pieceLength);
+            System.out.println("Piece Hashes:");
+            for (String hash : torrent.pieceHashes) {
+                System.out.println(hash);
+            }
         } else {
             System.out.println("Unknown command: " + command);
-        }
-    }
-
-    private static String decodeBencode(String bencodedString) {
-        if (Character.isDigit(bencodedString.charAt(0))) {
-            int firstColonIndex = 0;
-            for (int i = 0; i < bencodedString.length(); i++) {
-                if (bencodedString.charAt(i) == ':') {
-                    firstColonIndex = i;
-                    break;
-                }
-            }
-            int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
-        } else {
-            throw new RuntimeException("Only strings are supported at the moment");
         }
     }
 
--- src/main/java/TorrentInfo.java
+++ src/main/java/TorrentInfo.java
@@ -4,6 +4,8 @@ import com.dampcake.bencode.Type;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,25 +18,24 @@ class TorrentInfo {
 
     @SuppressWarnings("unchecked")
     public TorrentInfo(byte[] bytes) throws NoSuchAlgorithmException {
-        Bencode bencode1 = new Bencode(false);
-        Bencode bencode2 = new Bencode(true);
-        Map<String, Object> root = bencode1.decode(bytes, Type.DICTIONARY);
+        Bencode bencode = new Bencode(false);
+        Map<String, Object> root = bencode.decode(bytes, Type.DICTIONARY);
         Map<String, Object> info = (Map<String, Object>) root.get("info");
+
         announce = (String) root.get("announce");
-        length = (long) info.get("length");
-        pieceLength = (long) info.get("piece length");
+        length = ((Number) info.get("length")).longValue();
+        pieceLength = ((Number) info.get("piece length")).longValue();
 
         String piecesString = (String) info.get("pieces");
         byte[] piecesBytes = piecesString.getBytes();
         pieceHashes = new ArrayList<>();
         for (int i = 0; i < piecesBytes.length; i += 20) {
-            byte[] hash = new byte[20];
-            System.arraycopy(piecesBytes, i, hash, 0, 20);
+            byte[] hash = Arrays.copyOfRange(piecesBytes, i, i + 20);
             pieceHashes.add(bytesToHex(hash));
         }
 
         MessageDigest digest = MessageDigest.getInstance("SHA-1");
-        infoHash = digest.digest(bencode2.encode((Map<String, Object>) bencode2.decode(bytes, Type.DICTIONARY).get("info")));
+        infoHash = digest.digest(bencode.encode((Map<String, Object>) info));
     }
 
     private static String bytesToHex(byte[] bytes) {
