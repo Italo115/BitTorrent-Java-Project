42b113edc3dfc19aedeb127a86e9f0f731a7518f
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-18 16:10:17 +0200

    Decode bencoded Integer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,12 +8,13 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         String command = args[0];
-        String bencodedValue = args[1];
-        BencodeDecoder decoder = new BencodeDecoder(bencodedValue.getBytes(StandardCharsets.UTF_8));
+
+
         if ("decode".equals(command)) {
             Object decoded;
             try {
-
+                String bencodedValue = args[1];
+                BencodeDecoder decoder = new BencodeDecoder(bencodedValue.getBytes(StandardCharsets.UTF_8));
                 decoded = decoder.decode();
             } catch (RuntimeException e) {
                 System.out.println(e.getMessage());
@@ -22,7 +23,7 @@ public class Main {
             System.out.println(gson.toJson(decoded));
         } else if ("info".equals(command)) {
             byte[] torrentData = BencodeDecoder.readFile(args[1]);
-            decoder = new BencodeDecoder(torrentData);
+            BencodeDecoder decoder = new BencodeDecoder(torrentData);
             Map<String, Object> decodedDictionary = (Map<String, Object>) decoder.decode();
             BencodeDecoder.printTorrentInfo(decodedDictionary);
         } else {
