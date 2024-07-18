a5ed764da1e59766376aa5a9ed57f1abe397e50f
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-18 16:19:48 +0200

    Decode bencoded Integer

--- src/main/java/BencodeDecoder.java
+++ src/main/java/BencodeDecoder.java
@@ -143,24 +143,6 @@ public class BencodeDecoder {
             sb.append(String.format("%02x", b));
         }
         return sb.toString();
-    }
 
-    static String decodeBencode(String bencodedString) {
-        if (Character.isDigit(bencodedString.charAt(0))) {
-            int firstColonIndex = 0;
-            for (int i = 0; i < bencodedString.length(); i++) {
-                if (bencodedString.charAt(i) == ':') {
-                    firstColonIndex = i;
-                    break;
-                }
-            }
-            int length =
-                    Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-            return bencodedString.substring(firstColonIndex + 1,
-                    firstColonIndex + 1 + length);
-        } else {
-            throw new RuntimeException(
-                    "Only strings are supported at the moment");
-        }
     }
 }
